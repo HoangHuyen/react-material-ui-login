59021c9a4e2076087ea914e317029299a0339d1c
creativesuraj <suraj@surajsharma.net>
2020-08-25 12:38:01 +0530

    Updates

---
+++ precache-manifest.de479a29af91647cb419fd1cd9eddd77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cceda188cdd31fdf2bac6e4e3e14c48f",
+    "url": "/react-material-ui-login/index.html"
+  },
+  {
+    "revision": "fa33d00c6f57f0f6e41b",
+    "url": "/react-material-ui-login/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b726cb517314f705b1a",
+    "url": "/react-material-ui-login/static/js/2.d53a195b.chunk.js"
+  },
+  {
+    "revision": "fa33d00c6f57f0f6e41b",
+    "url": "/react-material-ui-login/static/js/main.07fb8d88.chunk.js"
+  },
+  {
+    "revision": "3ba8e1dc82261d2bca6c",
+    "url": "/react-material-ui-login/static/js/runtime-main.7d608fc9.js"
+  }
+]);
